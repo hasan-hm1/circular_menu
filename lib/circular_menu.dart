1d6811c10cd3752a6f534e5bd121ba81cdec3d6c
hasan-hm1 <63445282+hasan-hm1@users.noreply.github.com>
2020-05-02 17:09:06 +0300

    Update circular_menu.dart

--- lib/circular_menu.dart
+++ lib/circular_menu.dart
@@ -1,3 +1,4 @@
 library circular_menu;
 export 'package:circular_menu/src/circular_menu.dart';
 export 'package:circular_menu/src/circular_menu_item.dart';
+export 'package:circular_menu/src/multi_circular_menu.dart';
