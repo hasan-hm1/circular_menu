972f9c89ca8abb50fd95ae5def7a851a1c0f32eb
hasan-hm1 <63445282+hasan-hm1@users.noreply.github.com>
2020-05-02 17:10:04 +0300

    added multi_circular_menu.dart

---
+++ lib/src/multi_circular_menu.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+ 
+ import 'circular_menu.dart';
+
+class MultiCircularMenu extends StatelessWidget {
+  /// List of CircularMenu contains at least two CircularMenu objects.
+  final List<CircularMenu> menus;
+
+  /// widget holds actual page content
+  final Widget backgroundWidget;
+  const MultiCircularMenu({@required this.menus, this.backgroundWidget})
+      : assert(menus.length != 0, 'menus can not be empty list'),
+        assert(menus.length > 1,
+            'no need to use MultiCircularMenu you can directly use CircularMenu');
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        backgroundWidget ?? Container(),
+        ...menus,
+      ],
+    );
+  }
+}
